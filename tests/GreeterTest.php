68d99327154acf2fc5e01e70423ec360b106899d
asakura-kazumi <ruos56@gmail.com>
2015-08-19 22:46:02 +0500

    AutoCommit

--- tests/GreeterTest.php
+++ tests/GreeterTest.php
@@ -5,7 +5,7 @@ class GreeterTest extends PHPUnit_Framework_TestCase
   public function testGreeter()
   {
     $u = new Greeter();
-    $this->assertEquals("Hello World!" === $u->show());
+    $this->assertEquals("Hello World!", $u->show());
   }
 }
 
